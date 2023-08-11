862523d9b57cf375efd5b5f8ddbaf23b3dbb40bd
Quentin Nicot <q.nicot@icloud.com>
2023-08-11 18:41:38 +0200

    Animation Appartement terminée

--- kasa/src/pages/Appartements.js
+++ kasa/src/pages/Appartements.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Slider from 'react-slick';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importez les icônes FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
 import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 import apptData from '../data/appartement.json';
@@ -28,7 +28,6 @@ const Appartements = () => {
     slidesToScroll: 1,
   };
 
-  // Fonction pour générer les étoiles en fonction du rating
   const generateStars = (rating) => {
     const stars = [];
     for (let i = 1; i <= 5; i++) {
@@ -47,53 +46,57 @@ const Appartements = () => {
     <div>
       <Header />
       <main className='apptMain'>
-      <section className='apptContent'>
-        <Slider {...settings}className='slider-container'>
-          {appartement.pictures.map((picture, index) => (
-            <div key={index}>
-              <img className="slider-image" src={picture} alt={`Appartement ${index + 1}`} />
+        <section className='apptContent'>
+          <Slider {...settings} className='slider-container'>
+            {appartement.pictures.map((picture, index) => (
+              <div key={index}>
+                <img className="slider-image" src={picture} alt={`Appartement ${index + 1}`} />
+              </div>
+            ))}
+          </Slider>
+          <div className='apptTxt'>
+            <div className='title-host'>
+              <div className='title-loc'>
+                <h2>{appartement.title}</h2>
+                <p>Location: {appartement.location}</p>
+              </div>
+              <div className='host'>
+                <p>{appartement.host.name}</p>
+                <img className='hostpict' src={appartement.host.picture} alt={appartement.host.name} />
+              </div>
             </div>
-          ))}
-        </Slider>
-        <div className='apptTxt'>
-          <div className='title-host'>
-            <div className='title-loc'>
-              <h2>{appartement.title}</h2>
-              <p>Location: {appartement.location}</p>
+            <div className='tags-rate'>
+              <ul className='tags'>
+                {appartement.tags.map((tag, index) => (
+                  <li key={index}>{tag}</li>
+                ))}
+              </ul>
+              <div className="rating-stars">
+                {generateStars(appartement.rating)}
+              </div>
             </div>
-            <div className='host'>
-              <p>{appartement.host.name}</p>
-              <img className='hostpict' src={appartement.host.picture} alt={appartement.host.name} />
-            </div>
-          </div>
-          <div className='tags-rate'>
-            <ul className='tags'>
-              {appartement.tags.map((tag, index) => (
-                <li key={index}>{tag}</li>
-              ))}
-            </ul>
-            <div className="rating-stars">
-              {generateStars(appartement.rating)}
-            </div>
-          </div>
-          <div className='description-equipement'>
-            <div className='description-menu'>
-              <h3 onClick={() => setIsDescriptionOpen(!isDescriptionOpen)}>Description</h3>
-              {isDescriptionOpen && <p>{appartement.description}</p>}
-            </div>
-            <div className='equipement-menu'>
-              <h3 onClick={() => setIsEquipmentsOpen(!isEquipmentsOpen)}>Equipements</h3>
-              {isEquipmentsOpen && (
-                <ul>
+            <div className='description-equipement'>
+              <div className='description-menu'>
+                <h3 onClick={() => setIsDescriptionOpen(!isDescriptionOpen)}>
+                  Description
+                  <span className={`arrow ${isDescriptionOpen ? 'open' : ''}`}>&#x25B2;</span>
+                </h3>
+                <p className={`content ${isDescriptionOpen ? 'open' : ''}`}>{appartement.description}</p>
+              </div>
+              <div className='equipement-menu'>
+                <h3 onClick={() => setIsEquipmentsOpen(!isEquipmentsOpen)}>
+                  Equipements
+                  <span className={`arrow ${isEquipmentsOpen ? 'open' : ''}`}>&#x25B2;</span>
+                </h3>
+                <ul className={`content ${isEquipmentsOpen ? 'open' : ''}`}>
                   {appartement.equipments.map((equipment, index) => (
                     <li key={index}>{equipment}</li>
                   ))}
                 </ul>
-              )}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
       </main>
       <Footer />
     </div>
