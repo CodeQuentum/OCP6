d5fb386a0f3cecfa65e3a92d4b29fc469e31ca80
Quentin Nicot <q.nicot@icloud.com>
2023-08-11 15:35:08 +0200

    animation page about faite

--- kasa/src/pages/About.js
+++ kasa/src/pages/About.js
@@ -4,62 +4,59 @@ import Footer from '../components/Footer';
 import '../styles/About.scss';
 import imgAPropos from "../assets/img-apropos.png";
 
-const About = () => {
-  const [openParagraphs, setOpenParagraphs] = useState([false, false, false, false]);
+const Section = ({ title, content, isOpen, onClick }) => {
+  return (
+    <div className="menuDeroulant">
+      <h2 onClick={onClick}>
+        {title} <span className={`arrow ${isOpen ? 'open' : ''}`}>&#x25B2;</span>
+      </h2>
+      <p className={`content ${isOpen ? 'open' : ''}`}>{content}</p>
+    </div>
+  );
+};
 
-  const handleMenuClick = (index) => {
-    const updatedParagraphs = [...openParagraphs];
-    updatedParagraphs[index] = !updatedParagraphs[index];
-    setOpenParagraphs(updatedParagraphs);
+const About = () => {
+  const sections = [
+    {
+      title: "Fiabilité",
+      content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+    },
+    {
+      title: "Respect",
+      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      title: "Service",
+      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+    },
+    {
+      title: "Sécurité",
+      content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+    }
+  ];
+
+  const [openSections, setOpenSections] = useState(Array(sections.length).fill(false));
+
+  const handleSectionClick = (index) => {
+    const updatedSections = [...openSections];
+    updatedSections[index] = !updatedSections[index];
+    setOpenSections(updatedSections);
   };
 
   return (
     <div>
       <Header />
-      <main className='aboutMain'>
+      <main className="aboutMain">
         <img src={imgAPropos} alt="Lac bordée de montagne." />
-        <div className="menuDeroulant">
-          <h2 onClick={() => handleMenuClick(0)}>
-            Fiabilité 
-            <span className={`arrow ${openParagraphs[0] ? 'open' : ''}`}>&#x25B2;</span>
-          </h2>
-          {openParagraphs[0] && (
-            <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, 
-              et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
-          )}
-        </div>
-        <div className="menuDeroulant">
-          <h2 onClick={() => handleMenuClick(1)}>
-            Respect 
-            <span className={`arrow ${openParagraphs[1] ? 'open' : ''}`}>&#x25B2;</span>
-          </h2>
-          {openParagraphs[1] && (
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire 
-              ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-          )}
-        </div>
-        <div className="menuDeroulant">
-          <h2 onClick={() => handleMenuClick(2)}>
-            Service 
-            <span className={`arrow ${openParagraphs[2] ? 'open' : ''}`}>&#x25B2;</span>
-          </h2>
-          {openParagraphs[2] && (
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire 
-              ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-          )}
-        </div>
-        <div className="menuDeroulant">
-          <h2 onClick={() => handleMenuClick(3)}>
-            Sécurité 
-            <span className={`arrow ${openParagraphs[3] ? 'open' : ''}`}>&#x25B2;</span>
-          </h2>
-          {openParagraphs[3] && (
-            <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
-            correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-            locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-            également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-          )}
-        </div>
+        {sections.map((section, index) => (
+          <Section
+            key={index}
+            title={section.title}
+            content={section.content}
+            isOpen={openSections[index]}
+            onClick={() => handleSectionClick(index)}
+          />
+        ))}
       </main>
       <Footer />
     </div>
